20eb40eac7ea78e7d92d53ae851fec2813a30a50
Michael Murithi <mykemitrichev@gmail.com>
2023-10-27 22:52:16 +0200

    chore: setup render deployment

---
+++ render_build_backend.sh
@@ -0,0 +1,30 @@
+echo "-----> Build hook"
+
+echo "-----> Build frontend"
+npm install
+npm run build
+echo "-----> Build frontend done"
+
+echo "-----> Poetry install"
+poetry install --no-interaction --without dev
+chmod u+rwx backend/manage.py
+echo "-----> Poetry done"
+
+if [ -n "$ENABLE_DJANGO_COLLECTSTATIC" ] && [ "$ENABLE_DJANGO_COLLECTSTATIC" == 1 ]; then
+    echo "-----> Running collectstatic"
+    
+    echo "-----> Collecting static files"
+    poetry run backend/manage.py collectstatic --noinput  2>&1 | sed '/^Copying/d;/^$/d;/^ /d'
+    
+    echo
+fi
+
+echo "-----> Running manage.py check --deploy --fail-level WARNING"
+poetry run backend/manage.py check --deploy --fail-level WARNING
+
+if [ -n "$AUTO_MIGRATE" ] && [ "$AUTO_MIGRATE" == 1 ]; then
+    echo "-----> Running manage.py migrate"
+    poetry run backend/manage.py migrate --noinput
+fi
+
+echo "-----> Post-compile done"
