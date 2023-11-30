1a8eaa1fe69d18ff694b7e36c2a3f359393a1ed8
Michael Murithi <47428593+MichaelMurithi@users.noreply.github.com>
2023-11-30 22:14:28 +0100

    fix(solution): layout fixes (#20)
    * fix: fixed dropdown appearance
    
    * feat(solution): implemented order flow notifications (#17)
    
    * fix: fixed dropdown appearance
    
    * feat: installed sendpulse
    
    * feat: added cancel buttons to modals
    
    * feat: implemented password reset form
    
    * feat: implemented password validation
    
    * feat: implemented password reset UI
    
    * fix: backend cleanup
    
    * feat: added customer account activation to order flow
    
    * feat: disabled persisting of details for existing customers
    
    * fix: checkout cleanup
    
    * feat: added template config to render variables
    
    * fix: cleaned up email template
    
    * fix: applied UI feedback
    
    * fix: made allergens field optional
    
    * fix: implementing getting of username from email address
    
    * fix(solution): added sms notification for new orders (#19)
    
    * fix: fixed dropdown appearance
    
    * feat(solution): implemented order flow notifications (#17)
    
    * fix: fixed dropdown appearance
    
    * feat: installed sendpulse
    
    * feat: added cancel buttons to modals
    
    * feat: implemented password reset form
    
    * feat: implemented password validation
    
    * feat: implemented password reset UI
    
    * fix: backend cleanup
    
    * feat: added customer account activation to order flow
    
    * feat: disabled persisting of details for existing customers
    
    * fix: checkout cleanup
    
    * feat: added template config to render variables
    
    * fix: cleaned up email template
    
    * fix: applied UI feedback
    
    * fix: made allergens field optional
    
    * fix: implementing getting of username from email address
    
    * fixed cart dropdown issue
    
    * feat: added sms notifications on new order
    
    * feat: cleaned up login form
    
    * feat: installed analytics npm package
    
    * feat: added google analytics
    
    * feat(solution): implemented order flow notifications (#17)
    
    * fix: fixed dropdown appearance
    
    * feat: installed sendpulse
    
    * feat: added cancel buttons to modals
    
    * feat: implemented password reset form
    
    * feat: implemented password validation
    
    * feat: implemented password reset UI
    
    * fix: backend cleanup
    
    * feat: added customer account activation to order flow
    
    * feat: disabled persisting of details for existing customers
    
    * fix: checkout cleanup
    
    * feat: added template config to render variables
    
    * fix: cleaned up email template
    
    * fix: fixed styles
    
    * fix: added debugging for sms helper
    
    * fix: changed logger config

--- backend/common/utils/sms_helper.py
+++ backend/common/utils/sms_helper.py
@@ -1,23 +1,27 @@
+import logging
+
 from common.utils.config import Config
 from home.models import CompanyInfo
 from pysendpulse.pysendpulse import PySendPulse
 from shop.models import Order
 
 
+logger = logging.getLogger(__name__)
+
 TOKEN_STORAGE = "memcached"
 MEMCACHED_HOST = "127.0.0.1:11211"
-
-SendPulseAPIProxy = PySendPulse(
-    Config.get("SENDPULSE_API_ID"),
-    Config.get("SENDPULSE_API_SECRET"),
-    TOKEN_STORAGE,
-    memcached_host=MEMCACHED_HOST,
-)
 
 
 def send_new_order_sms(order: Order):
     try:
-        company_phone_number = CompanyInfo.objects.all().first().phone_numbers.split(",")[0]
+        SendPulseAPIProxy = PySendPulse(
+            Config.get("SENDPULSE_API_ID"),
+            Config.get("SENDPULSE_API_SECRET"),
+            TOKEN_STORAGE,
+            memcached_host=MEMCACHED_HOST,
+        )
+        company_phone_number = CompanyInfo.objects.all(
+        ).first().phone_numbers.split(",")[0]
         message = f"New order received from {order.customer_name()} at {order.customer_address()} phone number: {order.phone_number()}. Please check the admin panel for more details."
 
         res = SendPulseAPIProxy.sms_add_campaign_by_phones(
@@ -26,9 +30,10 @@ def send_new_order_sms(order: Order):
 
         try:
             if res["data"]["is_error"]:
-                print(f"SMS failed to send for {order}")
+                logger.error(f"SMS failed to send for {order}")
         except KeyError:
-            print(f"SMS sent for {order}")
-    except Exception:
-        print(f"An error occured while sending a notification for {order}")
+            logger.info(f"SMS sent for {order}")
+    except Exception as e:
+        logger.error(
+            f"An error occured while sending a notification for {order}: {str(e)}")
         return
