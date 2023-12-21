d033198bb49e912998f2c38f9b29ea2c31a5b1bb
Michael Murithi Kariuki <michael.kariuki@siemens-healthineers.com>
2023-12-21 10:19:05 +0100

    fix: fixed sms sending

--- backend/common/utils/sms_helper.py
+++ backend/common/utils/sms_helper.py
@@ -9,16 +9,9 @@ logger = logging.getLogger(__name__)
 
 TOKEN_STORAGE = "memcached"
 
-SendPulseAPIProxy = PySendPulse(
-    Config.get("SENDPULSE_API_ID"),
-    Config.get("SENDPULSE_API_SECRET"),
-    TOKEN_STORAGE,
-    memcached_host=Config.get("MEMCACHED_HOST"),
-)
-
 
 def send_new_order_sms(order):
-    SendPulseAPIProxy = PySendPulse(
+    send_pulse = PySendPulse(
         Config.get("SENDPULSE_API_ID"),
         Config.get("SENDPULSE_API_SECRET"),
         TOKEN_STORAGE,
@@ -36,7 +29,7 @@ def send_new_order_sms(order):
         order_notes = f",\nOrder notes: {order.order_notes}.\n" if order.order_notes else ".\n"
         message = f"New order received from {order.customer_name()} at {order.created_at.strftime('%I:%M %p')}:\n\nOrder items: {order.get_items()},\nPhone number: {order.phone_number()},\nPayment: {payment_details},\nDelivery: {delivery_details}{order_notes}\nPlease check the admin panel for more details,\nHappy cooking!"
 
-        res = SendPulseAPIProxy.sms_add_campaign_by_phones(
+        res = send_pulse.sms_add_campaign_by_phones(
             "Confessions", [company_phone_number], message
         )
 
@@ -52,10 +45,17 @@ def send_new_order_sms(order):
 
 
 def send_order_ready_sms(order):
+    send_pulse = PySendPulse(
+        Config.get("SENDPULSE_API_ID"),
+        Config.get("SENDPULSE_API_SECRET"),
+        TOKEN_STORAGE,
+        memcached_host=Config.get("MEMCACHED_HOST"),
+    )
+
     try:
         message = f"Hello {order.customer_short_name()},\n\nYour order from Cooking Confessions is ready for {'delivery' if order.delivery_mode == 1 else 'pickup'}.\n\nSee you soon!"
 
-        res = SendPulseAPIProxy.sms_add_campaign_by_phones(
+        res = send_pulse.sms_add_campaign_by_phones(
             "Confessions", [order.phone_number()], message
         )
 
